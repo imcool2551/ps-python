8938a8c8b886c9f05045cf783362159bf378b438
SangWook <imcool2551@gmail.com>
2022-01-04 11:50:56 +0900

    leetcode-344

--- leetcode/string/125.py
+++ leetcode/string/125.py
@@ -1,9 +1,11 @@
 import re
 
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         lower_alpha_numeric_string = re.sub('[^0-9a-zA-Z]', '', s.lower())
         reverse_string = lower_alpha_numeric_string[::-1]
         return lower_alpha_numeric_string == reverse_string
+
 
 print(Solution().isPalindrome("ab_a"))
--- leetcode/string/344.py
+++ leetcode/string/344.py
@@ -1,12 +1,8 @@
 class Solution:
-    def reverseString(self, s) -> None:
+    def reverseString(self, s: List[str]) -> None:
+        # s.reverse()
         left, right = 0, len(s) - 1
         while left < right:
             s[left], s[right] = s[right], s[left]
             left += 1
             right -= 1
-
-
-li = ["h", "e", "l", "l", "o"]
-print(Solution().reverseString(li))
-print(li)
